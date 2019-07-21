aac322970420a3a14ec099b25668cb42846c35a1
PanicMoon0808 <52909480+panicmoon0808@users.noreply.github.com>
2019-07-21 14:47:09 +0800

    Update urls.py

--- food_tracker/food/urls.py
+++ food_tracker/food/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import index, add_meal, delete_meal, update_meal, view_meal
+from .views import index, add_meal, delete_meal, update_meal, view_meal, login
 
 urlpatterns = [
-	url(r'^$', index, name='index'),
+	url(r'^$', login, name='login'),
+	url(r'index$', index, name='index'),
 	url(r'^add_meal$', add_meal, name='add-meal'),
 	url(r'^delete_meal/(?P<meal_id>\d+)$', delete_meal, name='delete-meal'),
 	url(r'^update_meal/(?P<meal_id>\d+)$', update_meal, name='update-meal'),
